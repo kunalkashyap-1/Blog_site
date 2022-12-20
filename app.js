6ebdc8b4fef3453f305160a0c1f4823759fb1ad1
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-12-20 23:21:17 +0530

    mongodb integrate with ejs

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const posts = [];
+// const posts = [];
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27020/blogDB");
+
+const blogSchema = {
+  Title:String,
+  content:String
+}
+
+const blogPost = mongoose.model("blogposts",blogSchema);
 
 app.set('view engine', 'ejs');
 
@@ -17,10 +27,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    "home_content": homeStartingContent,
-    "posts": posts
+  blogPost.find({},(err,result)=>{
+    if(!err){
+      res.render("home", {
+        "home_content": homeStartingContent,
+        "posts": result
+      });
+    }
   });
+  
 });
 
 app.get("/about", (req, res) => {
@@ -35,13 +50,11 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.get("/post/:day",(req,res)=>{
-  posts.forEach(elements=>{
-    if(_.lowerCase(elements.Title) === _.lowerCase(req.params.day)){
-      // console.log("match found");
-      res.render("post",{"curr_post":elements});
-    }
-    else{
+app.get("/post/:postid",(req,res)=>{
+  blogPost.findOne({_id : req.params.postid},(err,result)=>{
+    if(result){
+      res.render("post",{"curr_post":result});
+    }else{
       res.render("post",{"curr_post":{Title:"Blog Not Available",
       content:"Blog Not Available"}});
     }
@@ -50,12 +63,15 @@ app.get("/post/:day",(req,res)=>{
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const Post = new blogPost({
     Title: req.body.postTitle,
     content: req.body.post
-  }
-  posts.push(post);
-  res.redirect("/");
+  })
+  Post.save((err)=>{
+    if(!err){
+      res.redirect("/");
+    }
+  });
 
 });
 
